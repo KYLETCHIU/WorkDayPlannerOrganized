6f7b28d760aeb7b03fb935969f92515497e6e5d9
KYLETCHIU <92279620+KYLETCHIU@users.noreply.github.com>
2021-12-02 18:45:43 -0500

    Final touches
    Finished adding comments to the code to make understanding easier.

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -20,7 +20,7 @@ $(document).ready(function () {
         $(".time-block").each(function () {
             var stopTime = parseInt($(this).attr("id").split("hour")[1]);
 
-            //Working in excisting CSS and checking time
+            //Executes different CSS classes based on current time
 
             if (stopTime === currentTime) {
                 $(this).removeClass("future");
@@ -42,11 +42,12 @@ $(document).ready(function () {
         })
     }
 
-    //localStorage
+    //localStorage //AM
     $("hour8 .description").val(localStorage.getItem("hour8"));
     $("hour9 .description").val(localStorage.getItem("hour9"));
     $("hour10 .description").val(localStorage.getItem("hour10"));
     $("hour11 .description").val(localStorage.getItem("hour11"));
+    //PM
     $("hour12 .description").val(localStorage.getItem("hour12"));
     $("hour13 .description").val(localStorage.getItem("hour13"));
     $("hour14 .description").val(localStorage.getItem("hour14"));
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # WorkDayPlannerOrganized
 A simple planner for one's workday
+
+This scheduling app can be used to organize work day events and projects. 
+Will show past, present, and current hours via color coded text blocks. 
+All imputed text is saved to local storage.
+Enjoy!
